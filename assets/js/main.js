03c17bd53d418e77fe1c8118a30bccb16be2f6ed
SpedusaJurnalistik <spedusajurnalistik@gmail.com>
2023-05-12 09:28:19 +0700

    deed

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+
+
+function full(){
+    document.documentElement.requestFullscreen()
+}
+function tes1(){
+    setTimeout(function(){
+        let a = document.getElementById('tes')
+        a.click()
+    },1000)
+   
+}
+
+    
+
