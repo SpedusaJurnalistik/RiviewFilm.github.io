b8581c0a1c26f4d5aba17648aa377e862d7ab271
SpedusaJurnalistik <spedusajurnalistik@gmail.com>
2023-05-12 10:57:45 +0700

    wddwdw

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,9 @@
+setTimeout (function(){
+    $('#karakter').removeClass("animate__animated animate__fadeInUp")
+},1000)
 
 
-function full(){
-    document.documentElement.requestFullscreen()
-}
-function tes1(){
-    setTimeout(function(){
-        let a = document.getElementById('tes')
-        a.click()
-    },1000)
-   
-}
+
 
     
 
